7b2b446b216e33c09c2e4bfab479f65474e9fbfc
taniwhy <tanitani628@gmail.com>
2020-03-02 21:32:34 +0900

    add login handler

--- auth.go
+++ auth.go
@@ -1,11 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
 
- type authHandler struct {
-	 next http.Handler
- }
- 
+type authHandler struct {
+	next http.Handler
+}
+
 func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
 		w.Header().Set("Location", "/login")
@@ -20,3 +25,17 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func MustAuth(handler http.Handler) http.Handler {
 	return &authHandler{next: handler}
 }
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	log.Println(r.URL.Path)
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		log.Println("TODO: ログイン処理", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "アクション%sには非対応です", action)
+	}
+} 
